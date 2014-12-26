273db90bf08c367bd5e04b6665761b3684837a0d
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2014-12-26 10:43:27 +0000

    aufs: proc_map.patch

--- mm/memory.c
+++ mm/memory.c
@@ -2646,6 +2646,8 @@ reuse:
 		/* file_update_time outside page_lock */
 		if (vma->vm_file)
 			file_update_time(vma->vm_file);
+		if (vma->vm_prfile)
+			file_update_time(vma->vm_prfile);
 
 		return ret;
 	}
@@ -3326,6 +3328,8 @@ out:
 		/* file_update_time outside page_lock */
 		if (vma->vm_file)
 			file_update_time(vma->vm_file);
+		if (vma->vm_prfile)
+			file_update_time(vma->vm_prfile);
 	} else {
 		unlock_page(vmf.page);
 		if (anon)
